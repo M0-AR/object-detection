e1544d6c6c1be553fd86b668f9845737aab59809
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-03-30 14:23:37 +0200

    Use midjourney to generate new image then replace as subimage with the desire object on the image

--- main.py
+++ main.py
@@ -15,6 +15,7 @@
 # model.predict("home.jpg").save("prediction.jpg")
 
 # Replace os.getenv("HOME") with os.getenv("USERPROFILE") for Windows.
+"""
 from roboflow import Roboflow
 rf = Roboflow(api_key="")
 project = rf.workspace().project("window-detector")
@@ -28,4 +29,43 @@ model.predict("one-window.jpg", confidence=40, overlap=30).save("prediction.jpg"
 
 # infer on an image hosted elsewhere
 # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
+"""
+import time
+import cv2
+from midjourney_api import generate_image_by_midjourney
 
+# Load the original image and the new window design
+original_image = cv2.imread('one-window.jpg')
+generate_image_by_midjourney('just white window  width: 62.0 height: 82.0')
+
+time.sleep(2)
+new_window_design = cv2.imread('midjourney.png')
+
+# JSON response
+response = {
+    'predictions': [
+        {
+            'x': 198.0, 'y': 161.0, 'width': 62.0, 'height': 82.0, 'confidence': 0.6979602575302124, 'class': 'window', 'image_path': 'one-window.jpg', 'prediction_type': 'ObjectDetectionModel'
+        }
+    ], 'image': {'width': '460', 'height': '307'}
+}
+
+# Extract the detected window coordinates (center) and size
+x_center, y_center, width, height = int(response['predictions'][0]['x']), int(response['predictions'][0]['y']), int(response['predictions'][0]['width']), int(response['predictions'][0]['height'])
+
+# Calculate the top-left corner coordinates
+x, y = x_center - width // 2, y_center - height // 2
+
+# Resize the new window design to match the size of the detected window
+resized_window_design = cv2.resize(new_window_design, (width, height))
+
+# Replace the detected window in the original image with the resized new window design
+original_image[y:y+height, x:x+width] = resized_window_design
+
+# Save the modified image
+cv2.imwrite('modified_image.jpg', original_image)
+
+# Display the modified image (optional)
+cv2.imshow('Modified Image', original_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ midjourney_api.py
@@ -0,0 +1,48 @@
+# https://towardsdatascience.com/stable-diffusion-as-an-api-5e381aec1f6
+# import json
+# import base64
+#
+# import requests
+#
+#
+# def submit_post(url: str, data: dict):
+#     """
+#     Submit a POST request to the given URL with the given data.
+#     """
+#     return requests.post(url, data=json.dumps(data))
+#
+#
+# def save_encoded_image(b64_image: str, output_path: str):
+#     """
+#     Save the given image to the given output path.
+#     """
+#     with open(output_path, "wb") as image_file:
+#         image_file.write(base64.b64decode(b64_image))
+#
+#
+# if __name__ == '__main__':
+#     txt2img_url = 'http://127.0.0.1:7861/sdapi/v1/txt2img'
+#     data = {'prompt': 'a dog wearing a hat'}
+#     response = submit_post(txt2img_url, data)
+#     save_encoded_image(response.json()['images'][0], 'dog.png')
+
+
+import requests
+from PIL import Image
+from io import BytesIO
+
+
+def submit_post(url, data):
+    return requests.get(url, params=data)
+
+
+def save_encoded_image(encoded_image, file_name):
+    with open(file_name, 'wb') as f:
+        f.write(encoded_image)
+
+
+def generate_image_by_midjourney(prompt):
+    txt2img_url = 'http://65a4-34-143-140-24.ngrok.io'
+    data = {'prompt': prompt}
+    response = submit_post(txt2img_url, data)
+    save_encoded_image(response.content, 'midjourney.png')
